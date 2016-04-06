0564bfc733c63f0c4cc8aabdcf827cb8deb9636e
Vicente Canales <vcanales@smartboxtv.com>
2016-04-06 13:59:28 -0300

    Add missing break;s

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 module.exports = number => {
     switch(parseInt(number)) {
-        case 1: return "one";
-        case 2: return "two";
-        case 3: return "three";
-        case 4: return "four";
-        case 5: return "five";
-        case 6: return "six";
-        case 7: return "seven";
-        case 8: return "eight";
-        case 9: return "nine";
-        case 10: return "ten";
-        case 11: return "eleven";
-        case 12: return "twelve";
-        case 13: return "thirteen";
-        case 14: return "fourteen";
-        case 15: return "fifteen";
-        case 16: return "sixteen";
+        case 1: return "one"; break;
+        case 2: return "two"; break;
+        case 3: return "three"; break;
+        case 4: return "four"; break;
+        case 5: return "five"; break;
+        case 6: return "six"; break;
+        case 7: return "seven"; break;
+        case 8: return "eight"; break;
+        case 9: return "nine"; break;
+        case 10: return "ten"; break;
+        case 11: return "eleven"; break;
+        case 12: return "twelve"; break;
+        case 13: return "thirteen"; break;
+        case 14: return "fourteen"; break;
+        case 15: return "fifteen"; break;
+        case 16: return "sixteen"; break;
         default: return false;
     }
 }
